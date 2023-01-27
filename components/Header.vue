7e30484c39b06058ea033cc648a363c320923f23
Philip <pvu2002@outlook.com>
2023-01-26 20:19:04 -0800

    feat(app): Header and layouts setup

---
+++ components/Header.vue
@@ -0,0 +1,101 @@
+<template>
+  <Popover class="relative bg-white">
+    <div
+      class="flex items-center justify-between p-6 md:justify-start md:space-x-10"
+    >
+      <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="#">
+          <span class="sr-only">Your Company</span>
+          <Logo class="h-8 w-auto sm:h-10" />
+        </a>
+      </div>
+      <div class="-my-2 -mr-2 md:hidden">
+        <PopoverButton
+          class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+        >
+          <span class="sr-only">Open menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </PopoverButton>
+      </div>
+
+      <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+        <a
+          href="#"
+          class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+          >Sign in</a
+        >
+        <!-- class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700" -->
+
+        <button href="#" class="primary-button ml-8">Sign up</button>
+      </div>
+    </div>
+
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel
+        focus
+        class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+      >
+        <div
+          class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="px-5 pt-5 pb-6">
+            <div class="flex items-center justify-between">
+              <div>
+                <Logo class="h-8 w-auto" />
+              </div>
+              <div class="-mr-2">
+                <PopoverButton
+                  class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Close menu</span>
+                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+          </div>
+          <div class="py-6 px-5">
+            <div class="mt-6">
+              <!-- class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700" -->
+              <a href="#" class="primary-button w-full center !py-4"
+                >Sign up
+              </a>
+              <p class="mt-6 text-center text-base font-medium text-gray-500">
+                Existing customer?
+                {{ " " }}
+                <a href="#" class="text-salmon/80 hover:text-salmon">Sign in</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </PopoverPanel>
+    </transition>
+  </Popover>
+</template>
+
+<script setup lang="ts">
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/vue";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartBarIcon,
+  CursorArrowRaysIcon,
+  DocumentChartBarIcon,
+  ShieldCheckIcon,
+  Squares2X2Icon,
+  XMarkIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+
+<style scoped></style>
---
+++ components/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl text-purple font-display">Momentum</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
