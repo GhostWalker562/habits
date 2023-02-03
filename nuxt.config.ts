733d36c1638335697b915f7d27db7bec56a57d8e
Philip <pvu2002@outlook.com>
2023-02-02 21:21:12 -0800

    feat(app): Add Supabase to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    // "@nuxtjs/supabase",
+    "@nuxtjs/supabase",
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "@vueuse/nuxt",
