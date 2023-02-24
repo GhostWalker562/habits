6754939d10837ec4f8941e39d7fbe7d59391e1dd
Leonille Matunan <leonillematunan@gmail.com>
2023-02-23 20:04:33 -0800

    feat(app): Landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <div class="w-[650px]">
+  <div class="static">
+    <div class="relative sm:flex  md:w-[520px] left-10" >
       <h1 class="text-6xl text-purple font-extralight">
         Creating new habits, and fulfill your goals one day at a time
       </h1>
     </div>
+    <div class="relative left-16 top-8">
+    <h2 class="text-purple text-xl">Start tracking your habits now</h2>
+    </div>
+    <div class="absolute inset-x-0 bottom-0">
+      <hr class="w-full h-1 my-2 border-1s rounded md:my-10 dark:bg-salmon">
+      </div>
   </div>
 </template>
-
 <script setup lang="ts">
 definePageMeta({
   layout: "landing",
